dd66e0dab0ad073a2d196242de56b19ef0838b84
dsizzle67 <ahsokabooks@outlook.com>
2025-02-19 18:29:51 -0500

    button goes to book recommendations page

--- bookcataloging/views.py
+++ bookcataloging/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "bookcataloging/index.html"
+
 
 def book_recs(request):
     return render(request, 'bookcataloging/book_recs.html')
