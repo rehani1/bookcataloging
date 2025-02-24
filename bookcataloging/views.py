d0407e8067d18ca45496745012c815e1580e85dd
yuinacats@gmail.com <yuinacats@gmail.com>
2025-02-23 21:45:55 -0500

    made different views based off role
    Signed-off-by: yuinacats@gmail.com <yuinacats@gmail.com>

--- bookcataloging/views.py
+++ bookcataloging/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 
-def index_view(request):
+def get_role(request):
     user_role = ''
     if request.user.is_authenticated: 
         if request.user.groups.filter(name='Librarian').exists():
             user_role = 'Librarian'
         elif request.user.groups.filter(name='Patron').exists():
             user_role = 'Patron'
-            
-    context = {
-        'user_role': user_role,
-    }
 
-    return render(request, 'bookcataloging/index.html', context)
+    return user_role
+
+def index_view(request):
+    user_role = get_role(request)
+    return render(request, 'bookcataloging/index.html', {'user_role': user_role})
 
 def book_recs(request):
-    return render(request, 'bookcataloging/book_recs.html')
+    user_role = get_role(request)
+    return render(request, 'bookcataloging/book_recs.html', {'user_role': user_role})
 
 def profile_view(request):
-    return render(request, 'bookcataloging/profile.html')
+    user_role = get_role(request)
+    return render(request, 'bookcataloging/profile.html', {'user_role': user_role})
 
