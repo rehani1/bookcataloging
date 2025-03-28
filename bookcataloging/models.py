d2841f091c133ee2d35231a81fba6538d7768dd3
Aaron Park <135677875+aaronmpark@users.noreply.github.com>
2025-03-28 09:08:35 -0400

    added location for books and description for collections as attributes

--- bookcataloging/models.py
+++ bookcataloging/models.py
@@ -42,6 +42,7 @@ class Book(models.Model):
     book_image = models.ImageField(upload_to='book_pics/', blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books', null=True)
     series = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -141,6 +142,7 @@ class Collections(models.Model):
     books = models.ManyToManyField(Book, related_name='collections')
     is_public = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='collections')
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
