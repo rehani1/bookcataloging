524bdaf6a9a0e360551bfe196875fb38a69fd243
dsizzle67 <ahsokabooks@outlook.com>
2025-04-29 13:07:30 -0400

    test change for public/private collections

--- bookcataloging/views.py
+++ bookcataloging/views.py
@@ -256,8 +256,9 @@ def add_collection(request):
     if request.method == 'POST': # adds the collection
         name = request.POST.get('name')
         description = request.POST.get('description', '')
-        is_public = request.POST.get('is_public') != 'off'
-        
+        # is_public = request.POST.get('is_public') != 'off'
+        is_public = request.POST.get('is_public')
+
         if name:
             try:
                 Collections.objects.create(
